c03c781dcc100d4207663bc945cabfafb84610e1
Léo WADIN <wadinleo@gmail.com>
2023-10-24 16:42:39 +0200

    documentation

--- client.py
+++ client.py
@@ -1,52 +1,84 @@
+"""
+Authors: Léo WADIN & Aurélien HOUDART
+Date: 24 octobre 2023
+Version: 3.1
+
+Description:
+    Ce script réalise les étapes suivantes :
+    1. Établit une connexion TCP avec un dispatcher sur l'adresse et le port donnés.
+    2. Envoie une requête initiale ("i_have_a_request_please") au dispatcher.
+    3. Reçoit l'adresse et le port d'un worker du dispatcher.
+    4. Ferme la connexion avec le dispatcher.
+    5. Établit une nouvelle connexion TCP avec le worker.
+    6. Envoie une requête ("get_time") au worker.
+    7. Reçoit la réponse du worker, qui est censée être l'heure actuelle.
+    8. Ferme la connexion avec le worker.
+    
+    Note : Des timeouts sont ajoutés pour chaque opération réseau pour une meilleure robustesse.
+"""
+
 import socket
 import struct
-import time
+
 
 def main():
     host = '127.0.0.1'  # L'adresse du serveur
-    port = 42424       # Le port utilisé par le serveur
+    port = 42424        # Le port utilisé par le serveur
+    timeout = 5         # Timeout en secondes
 
-    # Socket pour la connexion au dispatcher
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    try:
-        s.connect((host, port))
-        local_address, local_port = s.getsockname()
-        print(f"Je suis {local_address}:{local_port}")
-    except ConnectionRefusedError:
-        print("Impossible de se connecter au serveur.")
-        return
+    # Crée un socket pour la connexion au dispatcher
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        
+        # Définir un timeout pour la connexion et les opérations de socket
+        s.settimeout(timeout)
+        
+        try:
+            # Tentative de connexion au dispatcher
+            s.connect((host, port))
+            local_address, local_port = s.getsockname()
+            print(f"Je suis {local_address}:{local_port}")
+        except (ConnectionRefusedError, socket.timeout):
+            print("Impossible de se connecter au serveur.")
+            return
 
-    # Définir l'option SO_LINGER à zéro pour une fermeture immédiate
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
+        # Définir l'option SO_LINGER à zéro pour une fermeture immédiate
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
 
-    s.sendall(b'i_have_a_request_please')
+        # Envoie la requête initiale au dispatcher
+        s.sendall(b'i_have_a_request_please')
 
-    # On attend les infos du worker
-    data = s.recv(1024)  # Attends la réponse
-    print(f"Reçu : {data.decode('utf-8')}")
-    addr, port = data.decode('utf-8').split(' ')
-    port = int(port)
+        try:
+            # Attends les infos du worker
+            data = s.recv(1024)
+        except socket.timeout:
+            print("Timeout lors de la réception des données.")
+            return
 
-    s.close()
+        print(f"Reçu : {data.decode('utf-8')}")
+        addr, port = data.decode('utf-8').split(' ')
+        port = int(port)
 
-    # Socket pour la connexion au worker
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((addr, port))
-    print(f"Je suis connecté à {addr}:{port}")
+    # Crée un socket pour la connexion au worker
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        
+        # Définir un timeout pour la connexion et les opérations de socket
+        s.settimeout(timeout)
 
-    #On envoie get_time au worker
-    s.sendall(b'get_time')
+        # Tentative de connexion au worker
+        s.connect((addr, port))
+        print(f"Je suis connecté à {addr}:{port}")
 
-    # On attend la réponse du worker
-    data = s.recv(1024)  # Attends la réponse
-    print(f"Reçu : {data.decode('utf-8')}")
-    s.close()
+        # Envoie la requête 'get_time' au worker
+        s.sendall(b'get_time')
 
+        try:
+            # Attends la réponse du worker
+            data = s.recv(1024)
+        except socket.timeout:
+            print("Timeout lors de la réception des données.")
+            return
 
-
-    
+        print(f"Reçu : {data.decode('utf-8')}")
 
 if __name__ == "__main__":
     main()
-
--- watchdog.py
+++ watchdog.py
@@ -1,3 +1,17 @@
+"""
+Authors: Léo WADIN & Aurélien HOUDART
+Date: 24 octobre 2023
+Version: 2.3
+
+Description:
+    Ce script agit en tant que watchdog pour des serveurs de type "dispatcher" et "worker".
+    Il réalise les tâches suivantes :
+    1. Crée des dossiers et pipes nécessaires s'ils n'existent pas.
+    2. Lance les serveurs "dispatcher" et "worker".
+    3. Surveille la santé de ces serveurs.
+    4. Arrête proprement les serveurs et nettoie en cas de signal de terminaison.
+"""
+
 import os
 import time
 import signal
@@ -10,24 +24,23 @@ from worker import serveur_worker
 dw_pipe_path = 'pipes/dwtube'
 wd_pipe_path = 'pipes/wdtube'
 
-already_killed = False
-
-if os.path.exists(dw_pipe_path):
-    os.remove(dw_pipe_path)
-if os.path.exists(wd_pipe_path):
-    os.remove(wd_pipe_path)
-
-# Crée les dossiers et pipes si besoin
-if not os.path.exists('pipes'):
-    os.makedirs('pipes')
-if not os.path.exists(dw_pipe_path):
-    os.mkfifo(dw_pipe_path)
-if not os.path.exists(wd_pipe_path):
-    os.mkfifo(wd_pipe_path)
-
+# Liste des PIDs des processus enfants
 child_pids = []
 
+# Flag pour la terminaison
+already_killed = False
+
+def create_pipes_and_dirs():
+    """Crée les dossiers et pipes nécessaires."""
+    if not os.path.exists('pipes'):
+        os.makedirs('pipes')
+    if not os.path.exists(dw_pipe_path):
+        os.mkfifo(dw_pipe_path)
+    if not os.path.exists(wd_pipe_path):
+        os.mkfifo(wd_pipe_path)
+
 def kill_children(signum, frame):
+    """Arrête tous les processus enfants."""
     global already_killed
     if already_killed:
         return
@@ -37,41 +50,47 @@ def kill_children(signum, frame):
         os.kill(pid, signal.SIGTERM)
     os.remove(dw_pipe_path)
     os.remove(wd_pipe_path)
-    
     sys.exit(0)
 
-def launch_server(func, args_tuple, child_pids):
+def launch_server(func, args_tuple):
+    """Lance un serveur et ajoute son PID à la liste."""
     pid = os.fork()
     if pid == -1:
         print("Erreur lors du fork.")
         sys.exit(1)
     elif pid == 0:
+        # Réinitialisation des gestionnaires de signaux pour les processus fils
+        # pour les faire revenir au comportement par défaut.
         signal.signal(signal.SIGTERM, signal.SIG_DFL)
         signal.signal(signal.SIGINT, signal.SIG_DFL)
         func(*args_tuple)
     else:
         print(f"Watchdog: J'ai lancé un serveur avec le PID {pid}.")
         child_pids.append(pid)
-        return pid
-    
+
 if __name__ == "__main__":
+    # Configuration des gestionnaires de signaux
     signal.signal(signal.SIGTERM, kill_children)
     signal.signal(signal.SIGINT, kill_children)
 
+    # Initialisation
     print("Je suis le watchdog.")
+    create_pipes_and_dirs()
 
+    # Mémoire partagée et pipes
     shared_mem = mmap.mmap(-1, 1024)
     pipe_in_dwtube = os.open(dw_pipe_path, os.O_RDONLY | os.O_NONBLOCK)
     pipe_out_dwtube = os.open(dw_pipe_path, os.O_WRONLY)
     pipe_in_wdtube = os.open(wd_pipe_path, os.O_RDONLY | os.O_NONBLOCK)
     pipe_out_wdtube = os.open(wd_pipe_path, os.O_WRONLY)
 
-
-    dispatcher_pid = launch_server(serveur_dispatcher, (shared_mem, pipe_out_dwtube, pipe_in_wdtube), child_pids)
-    worker_pid = launch_server(serveur_worker, (shared_mem, pipe_in_dwtube, pipe_out_wdtube), child_pids)
+    # Lancement des serveurs
+    launch_server(serveur_dispatcher, (shared_mem, pipe_out_dwtube, pipe_in_wdtube))
+    launch_server(serveur_worker, (shared_mem, pipe_in_dwtube, pipe_out_wdtube))
 
     print("Watchdog: Serveurs lancés.")
 
+    # Boucle de surveillance
     while True:
         time.sleep(5)  # Vérifie l'état toutes les 5 secondes
-        
+
